3d4ec5ee5983c8fc61d65c0c1893448f6c0017fa
nethranjalijayasanki <ssunflower620@gmail.com>
2024-06-20 09:14:02 +0530

    BO

--- Layered-Architecture-Project/src/main/java/com/example/layeredarchitecture/dao/OrderDetailDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.layeredarchitecture.dao;
-
-import com.example.layeredarchitecture.model.OrderDetailDTO;
-
-import java.sql.SQLException;
-
-public interface OrderDetailDAO {
-    boolean saveOrderDetails(String orderId, OrderDetailDTO detail) throws SQLException, ClassNotFoundException;
-}
